92e31d5f90344d88333c8e387e29aeb9e97f1e4e
Edwin Rajz <reiseddy@yahoo.com>
2021-05-03 11:08:09 +0300

    [WIP] now using FutureBuilder in orders page

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ import '../pages/edit_products_page.dart';
 
 void main() => runApp(ShopApp());
 
-//252
+//254
 class ShopApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
